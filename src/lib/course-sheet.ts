8f15d7eb6571838a01566fa9fcaa8a9a9b480a99
fyfmac <79295025@qq.com>
2025-10-08 18:01:34 +0800

    优化结构

--- src/lib/course-sheet.ts
+++ src/lib/course-sheet.ts
@@ -1,11 +1,11 @@
-import { read, utils } from "xlsx";
+import * as XLSX from "xlsx";
 
-async function courseSheet(file: File) {
+export default async function courseSheet(file: File) {
   const arrayBuffer = await file.arrayBuffer();
-  const workbook = read(arrayBuffer);
+  const workbook = XLSX.read(arrayBuffer);
   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  const sheet_data: string[][] = utils.sheet_to_json(worksheet, { header: 1 });
-  console.log(sheet_data);
+  const sheetData: string[][] = XLSX.utils.sheet_to_json(worksheet, {
+    header: 1,
+  });
+  console.log(sheetData);
 }
-
-export default courseSheet;
